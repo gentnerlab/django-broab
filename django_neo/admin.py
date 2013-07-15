4c5c06521f884efdef7b55f5febb5a8b124c6f3b
Justin Kiggins <justin.kiggins@gmail.com>
2013-07-15 14:35:32 -0700

    fixed trailing comma

--- django_neo/admin.py
+++ django_neo/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django_neo.models import Block, Segment
 from django_neo.models import RecordingChannelGroup, RecordingChannel, Unit
-from django_neo.models import AnalogSignal, SpikeTrain, Event,
+from django_neo.models import AnalogSignal, SpikeTrain, Event
 from django_neo.models import EventType
 
 admin.site.register(Block)
