da98b2ff65fa7d93f9111ea7f6f56872f3aa7f2e
Justin Kiggins <justin.kiggins@gmail.com>
2013-07-19 11:40:14 -0700

    added api

---
+++ django_neo/api/resources.py
@@ -0,0 +1,199 @@
+from tastypie import fields
+from tastypie.resources import ModelResource
+from django_neo.models import Block, Segment
+from django_neo.models import RecordingChannelGroup, RecordingChannel, Unit
+from django_neo.models import AnalogSignal, IrregularlySampledSignal, SpikeTrain, Event
+from django_neo.models import EventType
+from tastypie.serializers import Serializer
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
+
+class CustomSerializer(Serializer):
+    def format_datetime(self,data):
+        return data.isoformat()
+
+class NeoMeta:
+    filtering = {
+        'name': ALL,
+        'description': ALL,
+        'file_origin': ALL,
+    }
+
+class BlockResource(ModelResource):
+    segments = fields.ToManyField('django_neo.api.resources.SegmentResource',
+                                  'Segments',
+                                  null=True,
+                                  blank=True,
+                                  full=True,
+                                  )
+    recording_channel_groups = fields.ToManyField('django_neo.api.resources.RecordingChannelGroupResource',
+                                                  'recording_channel_groups',
+                                                  null=True,
+                                                  blank=True,
+                                                  full=True,
+                                                  )
+    class Meta():
+        queryset = Block.objects.all()
+        resource_name = 'block'
+        filtering = {
+            'name': ALL,
+            'description': ALL,
+            'file_origin': ALL,
+        }
+
+
+class SegmentResource(ModelResource):
+    block = fields.ToOneField(BlockResource,
+                              'block',
+                              null=True,
+                              blank=True,
+                              )
+    analogsignals = fields.ToManyField('django_neo.api.resources.AnalogSignalResource',
+                                        'analogsignals',
+                                        null=True,
+                                        blank=True,
+                                        )
+    irregularlysampledsignals = fields.ToManyField('django_neo.api.resources.IrregularlySampledSignalResource',
+                                                     'irregularlysampledsignals',
+                                                     null=True,
+                                                     blank=True,
+                                                     )
+    spiketrains = fields.ToManyField('django_neo.api.resources.SpikeTrainResource',
+                                      'spiketrains',
+                                      null=True,
+                                      blank=True,
+                                      )
+    events = fields.ToManyField('django_neo.api.resources.EventResource',
+                                'events',
+                                null=True,
+                                blank=True,
+                                )
+    class Meta:
+        queryset = Segment.objects.all()
+        resource_name = 'segment'
+        filtering = {
+            'name': ALL,
+            'description': ALL,
+            'file_origin': ALL,
+            'block': ALL_WITH_RELATIONS,
+        }
+
+class RecordingChannelGroupResource(ModelResource):
+    recording_channels = fields.ToManyField('django_neo.api.resources.RecordingChannelResource',
+                                            'recording_channels',
+                                            null=True,
+                                            blank=True,
+                                            full=True,
+                                            )
+    units = fields.ToManyField('django_neo.api.resources.UnitResource',
+                               'units',
+                               null=True,
+                               blank=True,
+                               )
+    class Meta:
+        queryset = RecordingChannelGroup.objects.all()
+        resource_name = 'recording_channel_group'
+
+class RecordingChannelResource(ModelResource):
+    analog_signals = fields.ToManyField('django_neo.api.resources.AnalogSignalResource',
+                                        'analog_signals',
+                                        null=True,
+                                        blank=True,
+                                        )
+    recording_channel_groups = fields.ToManyField('django_neo.api.resources.RecordingChannelGroupResource',
+                                                  'recording_channel_groups',
+                                                  null=True,
+                                                  blank=True,
+                                                  )
+    class Meta:
+        queryset = RecordingChannel.objects.all()
+        resource_name = 'recording_channel'
+        filtering = {
+            'name': ALL,
+            'description': ALL,
+            'file_origin': ALL,
+            'block': ALL_WITH_RELATIONS,
+        }
+
+class UnitResource(ModelResource):
+    recording_channel_group = fields.ToOneField('django_neo.api.resources.RecordingChannelGroupResource','recording_channel_groups')
+    spike_trains = fields.ToManyField('django_neo.api.resources.SpikeTrainResource','spike_trains')
+    class Meta:
+        queryset = Unit.objects.all()
+        resource_name = 'unit'
+        filtering = {
+            'name': ALL,
+            'description': ALL,
+            'file_origin': ALL,
+            'spike_trains': ALL_WITH_RELATIONS,
+            'recording_channel_group': ALL_WITH_RELATIONS,
+        }
+
+class AnalogSignalResource(ModelResource):
+    segment = fields.ToOneField('django_neo.api.resources.SegmentResource','segments')
+    recording_channel = fields.ToOneField('django_neo.api.resources.RecordingChannelResource','recording_channels')
+    class Meta:
+        queryset = AnalogSignal.objects.all()
+        resource_name = 'analog_signal'
+        filtering = {
+            'name': ALL,
+            'description': ALL,
+            'file_origin': ALL,
+            'segment': ALL_WITH_RELATIONS,
+        }
+
+class IrregularlySampledSignalResource(ModelResource):
+    segment = fields.ToOneField('django_neo.api.resources.SegmentResource','segments')
+    class Meta:
+        queryset = IrregularlySampledSignal.objects.all()
+        resource_name = 'irregularly_sampled_signal'
+        filtering = {
+            'name': ALL,
+            'description': ALL,
+            'file_origin': ALL,
+            'segment': ALL_WITH_RELATIONS,
+        }
+
+class SpikeTrainResource(ModelResource):
+    segment = fields.ToOneField('django_neo.api.resources.SegmentResource','segment')
+    unit = fields.ToOneField('django_neo.api.resources.UnitResource',
+                             'units',
+                             null=True,
+                             blank=True,
+                             )
+    class Meta:
+        queryset = SpikeTrain.objects.all()
+        resource_name = 'spike_train'
+        filtering = {
+            'name': ALL,
+            'description': ALL,
+            'file_origin': ALL,
+            'segment': ALL_WITH_RELATIONS,
+            'unit': ALL_WITH_RELATIONS
+        }
+
+class EventTypeResource(ModelResource):
+    events = fields.ToManyField('django_neo.api.resources.EventResource','events')
+    class Meta:
+        queryset = EventType.objects.all()
+        resource_name = 'event_type'
+        filtering = {
+            'name': ALL,
+            'description': ALL,
+            'file_origin': ALL,
+            'segment': ALL_WITH_RELATIONS,
+            'events': ALL_WITH_RELATIONS,
+        }
+
+class EventResource(ModelResource):
+    segment = fields.ToOneField('django_neo.api.resources.SegmentResource','segments')
+    event_type = fields.ToOneField('django_neo.api.resources.EventTypeResource','event_types')
+    class Meta:
+        queryset = Event.objects.all()
+        resource_name = 'event'
+        filtering = {
+            'name': ALL,
+            'description': ALL,
+            'file_origin': ALL,
+            'segment': ALL_WITH_RELATIONS,
+            'event_type': ALL,
+        }
