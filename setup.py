ca96c49a0f59035364548ae5223c6042022b025a
Justin Kiggins <justin.kiggins@gmail.com>
2013-07-31 11:50:20 -0700

    fixing setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-neo',
+    name='django-mr-anderson',
     version='0.1',
     requires=[
         # 'djorm-ext-pgarray>=0.6',
@@ -18,7 +18,7 @@ setup(
     license='BSD License',
     description='A simple Django app to maintain objects from Python-Neo.',
     long_description=README,
-    url='https://github.com/gentnerlab/django-neo',
+    url='https://github.com/gentnerlab/django-mr-anderson',
     author='Justin Kiggins',
     author_email='justin.kiggins@gmail.com',
     classifiers=[
@@ -28,7 +28,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
